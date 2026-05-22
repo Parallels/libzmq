d736c5be4802f91e4a1de4bf05ef9a6a9d608887
Marco Borg <marcob14@users.noreply.github.com>
2026-05-22 16:08:14 +0200

    Backport 4.3.1 clock fixes and update platform build files

--- src/v1_decoder.cpp
+++ src/v1_decoder.cpp
@@ -114,11 +114,13 @@ int zmq::v1_decoder_t::eight_byte_size_ready ()
         return -1;
     }
 
+#if !defined(__aarch64__) && !defined(__x86_64__)
     //  Message size must fit within range of size_t data type.
     if (payload_length - 1 > std::numeric_limits <size_t>::max ()) {
         errno = EMSGSIZE;
         return -1;
     }
+#endif
 
     const size_t msg_size = static_cast <size_t> (payload_length - 1);
 
